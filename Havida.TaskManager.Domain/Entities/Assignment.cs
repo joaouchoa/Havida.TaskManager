c26433a733ee35228987accbe85394feee84c414
joao uchoa <joaodep2@gmail.com>
2024-12-17 16:19:10 -0300

    feat: creating domain

---
+++ Havida.TaskManager.Domain/Entities/Assignment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havida.TaskManager.Domain.Entities
+{
+    public class Assignment
+    {
+        public Guid Id { get; set; } 
+        public string Title { get; set; } 
+        public string Description { get; set; } 
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; 
+        public DateTime? CompletedAt { get; set; } 
+
+
+        public Guid UserId { get; set; }
+        public User User { get; set; } 
+    }
+}
